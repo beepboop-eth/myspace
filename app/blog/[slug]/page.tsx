235b0189bf9ff41a645cf8f9d62526faadadcf63
Janus Manlapaz <janus.manlapaz@uconn.edu>
2023-08-10 10:34:03 -0500

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -12,21 +12,24 @@ interface Props {
 }
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+console.log("apiUrl", apiUrl)
 
 export async function generateStaticParams() {
-  const posts: Post[] = await fetch(`${apiUrl}/api/content`).then((res) => res.json())
+  // const posts: Post[] = await fetch(`${apiUrl}/api/content`).then((res) => res.json())
+  const posts: Post[] = []
   
   return posts.map((post) => ({ params: { slug: post.slug } }))  
 }
 
 export default async function BlogPost({  params }: Props) {
-  const posts: Post[] = await fetch(`${apiUrl}/api/content`).then((res) => res.json())
+  // const posts: Post[] = await fetch(`${apiUrl}/api/content`).then((res) => res.json())
+  const posts: Post[] = []
 
   const post = posts.find((post) => post.slug === params.slug)!;
   return (
     <div>
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
+      <h1>{post?.title}</h1>
+      <p>{post?.content}</p>
     </div>
   )
 }
